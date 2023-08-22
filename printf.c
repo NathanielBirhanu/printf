1ece98b983ab745dacb04a4b862bdaf2c27fbc91
NathanielBirhanu <nbirhanu98@gmail.com>
2023-08-22 12:54:48 -0700

    update

--- printf.c
+++ printf.c
@@ -12,7 +12,6 @@ int _printf(const char *format, ...)
 	va_list args;
 
 	va_start(args, format);
-
 	while (*format != '\0')
 	{
 		if (*format == '%')
@@ -20,32 +19,33 @@ int _printf(const char *format, ...)
 			format++;
 			if (*format == '%')
 			{
-				write(STDOUT_FILENO, "%", 1);
-				count++;
+				write(STDOUT_FILENO, "%", 1); count++;
 			}
 			else if (*format == 'c')
 			{
 				int ch = va_arg(args, int);
 
-				write(STDOUT_FILENO, &ch, 1);
-				count++;
+				write(STDOUT_FILENO, &ch, 1); count++;
 			}
 			else if (*format == 's')
 			{
 				char *str = va_arg(args, char *);
 				int len = strlen(str);
 
-				write(STDOUT_FILENO, str, len);
-				count += len;
+				write(STDOUT_FILENO, str, len); count += len;
+			}
+			else if (*format == 'd' || *format == 'i')
+			{
+				int x = va_arg(args, int);
+
+				write(STDOUT_FILENO, &x, 1); count++;
 			}
 		}
 		else
 		{
-			write(STDOUT_FILENO, format, 1);
-			count++;
+			write(STDOUT_FILENO, format, 1); count++;
 		}
 		format++;
 	}
-	va_end(args);
-	return (count);
+	va_end(args); return (count);
 }
